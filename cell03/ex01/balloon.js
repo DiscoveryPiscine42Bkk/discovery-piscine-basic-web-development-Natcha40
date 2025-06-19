57fdc8f0ee01b6e9351d0b7a1f183f7f4ba0d13b
Natcha40 <njungsit@student.42bangkok.com>
2025-06-19 04:17:07 +0000

    1

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,9 @@
+// Get the balloon element
+const balloon = document.getElementById("balloon");
+
+//Initial state for ballon size and color
+let size = 200;
+let colorOrder = ["red", "green", "blue"];
+let colorIndex = 0;
+
+// Function to handle ballooon click
